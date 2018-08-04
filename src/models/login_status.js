99738b0dbb134f4199ed7c71e6a1b56e17622d56
dotSlashLu <dotslash.lu@gmail.com>
2018-08-05 00:42:19 +0800

    models/login_status: prevent module loading after location is set

--- src/models/login_status.js
+++ src/models/login_status.js
@@ -5,12 +5,18 @@ import config from "../config"
 export default () => {
 	return http.get(config.apiBase + "/user/status")
 	.then(resp => {
-		if (!resp.data.login && !window.location.pathname.endsWith("login.html"))
-			window.location.href = "/login.html"
+		if (!resp.data.login && 
+			!window.location.pathname.endsWith("login.html")) {
+			console.log("not logged in, should jump to login page")
+			throw("not logged in")
+		}
 	})
 	.catch(err => {
-		console.error(err)
+		// console.error(err)
 		if (!window.location.pathname.endsWith("login.html"))
 			window.location.href = "/login.html"
+		// throw an uncaught exception to stop the execution of the promise 
+		// chain
+		throw("not logged in")
 	})
 }
